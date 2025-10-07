fac0966eed52bcb5f768ed1e3ed6a0383f753f44
Rezaul Karim Tushar <53859459+RkTushar@users.noreply.github.com>
2025-10-07 15:09:37 +0600

    Update whatsapp.dart

--- nec_app/lib/widgets/whatsapp.dart
+++ nec_app/lib/widgets/whatsapp.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 /// Circular WhatsApp-style send button with light green background and no shadow.
-class WhatsAppButton extends StatelessWidget {
+class WhatsAppButton extends StatefulWidget {
   /// Called when the button is tapped.
   final VoidCallback? onTap;
 
@@ -20,6 +20,15 @@ class WhatsAppButton extends StatelessWidget {
   /// Asset path to the WhatsApp image.
   final String assetPath;
 
+  /// How long each shake animation lasts.
+  final Duration shakeDuration;
+
+  /// How often the shake should occur (time between shakes).
+  final Duration shakeInterval;
+
+  /// Maximum rotation in radians for the shake.
+  final double shakeAmplitude;
+
   const WhatsAppButton({
     super.key,
     this.onTap,
@@ -28,34 +37,96 @@ class WhatsAppButton extends StatelessWidget {
     this.iconColor = Colors.white,
     this.semanticLabel = 'Send on WhatsApp',
     this.assetPath = 'assets/images/whatsapp_logo.png',
+    this.shakeDuration = const Duration(milliseconds: 700),
+    this.shakeInterval = const Duration(seconds: 3),
+    this.shakeAmplitude = 0.18, // ~10 degrees
   });
 
   @override
+  State<WhatsAppButton> createState() => _WhatsAppButtonState();
+}
+
+class _WhatsAppButtonState extends State<WhatsAppButton> with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<double> _rotation;
+  bool _isDisposed = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: widget.shakeDuration,
+    );
+
+    // A quick shake sequence: center -> left -> right -> left -> center
+    _rotation = TweenSequence<double>([
+      TweenSequenceItem(tween: Tween(begin: 0.0, end: -widget.shakeAmplitude).chain(CurveTween(curve: Curves.easeOut)), weight: 1),
+      TweenSequenceItem(tween: Tween(begin: -widget.shakeAmplitude, end: widget.shakeAmplitude).chain(CurveTween(curve: Curves.easeInOut)), weight: 2),
+      TweenSequenceItem(tween: Tween(begin: widget.shakeAmplitude, end: -widget.shakeAmplitude / 2).chain(CurveTween(curve: Curves.easeInOut)), weight: 2),
+      TweenSequenceItem(tween: Tween(begin: -widget.shakeAmplitude / 2, end: 0.0).chain(CurveTween(curve: Curves.easeOut)), weight: 1),
+    ]).animate(_controller);
+
+    _scheduleNextShake();
+  }
+
+  Future<void> _scheduleNextShake() async {
+    while (!_isDisposed) {
+      await Future.delayed(widget.shakeInterval);
+      if (_isDisposed) break;
+      try {
+        await _controller.forward(from: 0);
+      } catch (_) {
+        // no-op if disposed mid-animation
+      }
+      if (_isDisposed) break;
+      _controller.reset();
+    }
+  }
+
+  @override
+  void dispose() {
+    _isDisposed = true;
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final double iconSize = size * 0.54; // keep proportions similar to a FAB
+    final double iconSize = widget.size * 0.60; // keep proportions similar to a FAB
 
     return Semantics(
-      label: semanticLabel,
+      label: widget.semanticLabel,
       button: true,
       child: Material(
         type: MaterialType.transparency,
         child: InkWell(
-          onTap: onTap ?? () { debugPrint('WhatsApp button tapped'); },
+          onTap: widget.onTap ?? () { debugPrint('WhatsApp button tapped'); },
           customBorder: const CircleBorder(),
           child: Container(
-            width: size,
-            height: size,
+            width: widget.size,
+            height: widget.size,
             decoration: BoxDecoration(
-              color: backgroundColor,
+              color: widget.backgroundColor,
               shape: BoxShape.circle,
             ),
             alignment: Alignment.center,
-            child: ColorFiltered(
-              colorFilter: ColorFilter.mode(iconColor, BlendMode.srcIn),
-              child: Image.asset(
-                assetPath,
-                width: iconSize,
-                height: iconSize,
+            child: AnimatedBuilder(
+              animation: _controller,
+              builder: (context, child) {
+                return Transform.rotate(
+                  angle: _rotation.value,
+                  child: child,
+                );
+              },
+              child: ColorFiltered(
+                colorFilter: ColorFilter.mode(widget.iconColor, BlendMode.srcIn),
+                child: Image.asset(
+                  widget.assetPath,
+                  width: iconSize,
+                  height: iconSize,
+                ),
               ),
             ),
           ),
